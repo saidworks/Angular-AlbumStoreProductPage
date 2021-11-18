19788a05563a4f80ea5a60f22c72820151bf5fee
Said ZITOUNI <zitouni.sd@gmail.com>
2021-11-18 21:58:45 +0100

    product service implement

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Http,Response} from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ProductService {
+  private _albumURL:string = "../assets/album.json";
+  constructor(private _http:Http,) {
+   }
+   getAlbum(id:number):Observable<any>{
+      return this._http.get(this._albumURL).map((response)=>response.json())
+   }
+}
