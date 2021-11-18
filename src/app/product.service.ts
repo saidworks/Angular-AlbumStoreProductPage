35e8a49ef14acae00ad20320bf78eee39f1c12f8
Said ZITOUNI <zitouni.sd@gmail.com>
2021-11-18 22:33:39 +0100

    add interface album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http,Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs';
+import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
 
 @Injectable()
